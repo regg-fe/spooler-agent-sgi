2983cb4b3c060651cc35bbdb837672777d7d32c4
regg-fe <robertvzla7@gmail.com>
2026-05-30 00:10:25 -0400

    feat: Configuracion del entorno de pruebas en local

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// 1. Lógica central de tu Spooler Agent (Ejemplo)
+pub fn validar_id_impresion(id: &str) -> bool {
+    id.starts_with("SPOOL-")
+}
+
+// 2. Módulo de pruebas requerido para cumplir con tu suite mínima [cite: 46]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_id_valido() {
+        assert!(validar_id_impresion("-12345"));
+    }
+
+    #[test]
+    fn test_id_invalido() {
+        assert!(!validar_id_impresion("PRINT-12345"));
+    }
+}
+
