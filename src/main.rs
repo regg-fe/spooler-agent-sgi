85c74810709af22ac057033ae32b0f7539ee6927
regg-fe <robertvzla7@gmail.com>
2026-05-30 20:06:25 -0400

    feat: configurando funcion prueba de suma

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("El resultado de la suma es: {}", plus(12, 12));
 }
 
+pub fn plus(a:i32, b:i32) -> i32 {
+    a+b
+}
 
 // 1. Lógica central de tu Spooler Agent (Ejemplo)
 pub fn validar_id_impresion(id: &str) -> bool {
